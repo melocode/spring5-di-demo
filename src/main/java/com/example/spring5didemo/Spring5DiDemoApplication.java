c5c1594b8f1d35e40cd802c5db9eef8fa1796c08
Lemuel <lemuel@lemuel>
2018-12-24 18:10:33 -0800

    init commit

---
+++ src/main/java/com/example/spring5didemo/Spring5DiDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring5didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5DiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5DiDemoApplication.class, args);
+    }
+
+}
+
